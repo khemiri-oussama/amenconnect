6b1b214485e4c478d4270f3db4563914428c183e
nexusMAKO1 <93985031+nexusMAKO1@users.noreply.github.com>
2025-01-19 17:03:02 +0000

    home

--- AmenConnect/src/pages/Home.tsx
+++ AmenConnect/src/pages/Home.tsx
@@ -5,10 +5,10 @@ import {
   IonButton,
   IonText,
   IonLabel,
-  IonToolbar,
-  IonTitle,
+  IonImg
 } from '@ionic/react';
 import { useState } from 'react';
+
 import './Home.css';
 
 export default function LoginPage() {
@@ -26,49 +26,55 @@ export default function LoginPage() {
       <IonContent className="ion-padding login-container" fullscreen>
         <div className="content-wrapper">
           <div className="login-box">
-            <h1 className="title">Bienvenu</h1>
-            <p className="subtitle">
-              Veuillez saisir les détails de votre compte
-            </p>
+            <div className="logo-container">
+              <IonImg src="../public/amen_logo.png" alt="Logo" className="logo" />
+            </div>
+            <div className="form-container">
+              <h1 className="title">Bienvenu</h1>
+              <p className="subtitle">
+                Veuillez saisir les détails de votre compte
+              </p>
 
-            <form onSubmit={handleLogin} className="login-form">
-              <div className="input-group">
-                <IonLabel className="input-label">Identifiant</IonLabel>
-                <IonInput
-                  type="email"
-                  value={email}
-                  onIonChange={e => setEmail(e.detail.value!)}
-                  placeholder="foulen@gmail.com"
-                  className="custom-input"
-                  required
-                />
-              </div>
+              <form onSubmit={handleLogin} className="login-form">
+                <div className="input-group">
+                  <IonLabel className="input-label">Identifiant</IonLabel>
+                  <IonInput
+                    type="email"
+                    value={email}
+                    onIonChange={e => setEmail(e.detail.value!)}
+                    placeholder="foulen@gmail.com"
+                    className="custom-input"
+                    required
+                  />
+                </div>
 
-              <div className="input-group">
-                <IonLabel className="input-label">Mot De Passe</IonLabel>
-                <IonInput
-                  type="password"
-                  value={password}
-                  onIonChange={e => setPassword(e.detail.value!)}
-                  className="custom-input"
-                  required
-                />
-              </div>
+                <div className="input-group">
+                  <IonLabel className="input-label">Mot De Passe</IonLabel>
+                  <IonInput
+                    type="password"
+                    value={password}
+                    onIonChange={e => setPassword(e.detail.value!)}
+                    className="custom-input"
+                    required
+                  />
+                </div>
 
-              <IonText className="forgot-password">
-                <a href="/forgot-password">Mot De Passe Oublié</a>
-              </IonText>
+                <IonText className="forgot-password">
+                  <a href="/forgot-password">Mot De Passe oublier ?</a>
+                </IonText>
 
-              <IonButton 
-                expand="block" 
-                type="submit"
-                className="login-button"
-              >
-                Se Connecter
-              </IonButton>
-            </form>
+                <IonButton 
+                  expand="block" 
+                  type="submit"
+                  className="login-button"
+                >
+                  Se Connecter
+                </IonButton>
+              </form>
+            </div>
           </div>
         </div>
+        <div className="blurry-div"></div>
       </IonContent>
     </IonPage>
   );
