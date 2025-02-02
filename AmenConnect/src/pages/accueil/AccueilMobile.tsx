d83d8e2a32fd879111735d5ca342e5566cafdeea
nexusMAKO1 <93985031+nexusMAKO1@users.noreply.github.com>
2025-02-02 11:35:44 +0000

    Accueil

--- AmenConnect/src/pages/accueil/AccueilMobile.tsx
+++ AmenConnect/src/pages/accueil/AccueilMobile.tsx
@@ -1,14 +1,9 @@
 import type React from "react"
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { IonContent, IonPage} from "@ionic/react"
 
 const AccueilMobile: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Accueil (Mobile)</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent className="ion-padding">
         <h2>Welcome to the Mobile Version</h2>
         <p>This is the mobile-specific content for the Accueil page.</p>
