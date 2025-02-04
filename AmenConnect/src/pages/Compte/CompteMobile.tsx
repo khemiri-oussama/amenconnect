01e97e44e88f0a091d1db892f747c489aefa57f4
nexusMAKO1 <93985031+nexusMAKO1@users.noreply.github.com>
2025-02-04 17:04:26 +0000

    accueil

--- AmenConnect/src/pages/Compte/CompteMobile.tsx
+++ AmenConnect/src/pages/Compte/CompteMobile.tsx
@@ -6,7 +6,7 @@ import { homeOutline, walletOutline, chatbubbleOutline, cardOutline, arrowForwar
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid } from "recharts";
 
 import "./CompteMobile.css"
-
+import NavMobile from "../../components/NavMobile"
 const chartData = [
   { month: "Jan", income: 2, expenses: 1 },
   { month: "Feb", income: 4, expenses: 2 },
@@ -130,31 +130,8 @@ const ComptePage: React.FC = () => {
             </div>
           </div>
         )}
-
-        {/* Bottom Navigation */}
-        <div className="bottom-tabs">
-          <button className="tab-button" onClick={() => history.push("/accueil")}>
-            <IonIcon icon={homeOutline} />
-            <span>Accueil</span>
-          </button>
-          <button className="tab-button active">
-            <IonIcon icon={walletOutline} />
-            <span>Compte</span>
-          </button>
-          <button className="tab-button" onClick={() => history.push('/ChatBot')}>
-            <IonIcon icon={chatbubbleOutline} />
-            <span>Chat</span>
-          </button>
-          <button className="tab-button" onClick={() => history.push("/Carte")}>
-            <IonIcon icon={cardOutline} />
-            <span>Carte</span>
-          </button>
-          <button className="tab-button">
-            <IonIcon icon={arrowForward} />
-            <span>Virements</span>
-          </button>
-        </div>
       </IonContent>
+      <NavMobile currentPage="compte" />
     </IonPage>
   )
 }
