dd5a3a853ac8f2ff5127e653da84b70ce4084dec
khemiri-oussama <76531196+khemiri-oussama@users.noreply.github.com>
2025-02-05 16:24:45 +0000

    virementMobile

---
+++ AmenConnect/src/pages/virement/virementMobile.tsx
@@ -0,0 +1,70 @@
+import type React from "react"
+import { useState } from "react"
+import { IonContent, IonPage, IonIcon, IonSearchbar, IonLabel, IonSegment, IonSegmentButton } from "@ionic/react"
+import {
+  cashOutline,
+  businessOutline,
+} from "ionicons/icons"
+import { useHistory } from "react-router-dom"
+import "./virementMobile.css"
+import NavMobile from "../../components/NavMobile"
+
+const VirementsMobile: React.FC = () => {
+  const history = useHistory()
+  const [selectedSegment, setSelectedSegment] = useState("historique")
+
+  return (
+    <IonPage>
+      <IonContent fullscreen className="ion-padding-horizontal dark-theme">
+        <div className="status-bar"></div>
+
+        <h1 className="page-title">Virements</h1>
+
+        {/* Transfer Options */}
+        <div className="transfer-options">
+          <button className="transfer-card" onClick={() => history.push("/beneficiary-transfer")}>
+            <div className="transfer-icon">
+              <IonIcon icon={cashOutline} />
+            </div>
+            <p>Effectuer un virement vers un bénéficiaire</p>
+          </button>
+
+          <button className="transfer-card" onClick={() => history.push("/account-transfer")}>
+            <div className="transfer-icon">
+              <IonIcon icon={businessOutline} />
+            </div>
+            <p>Effectuer un virement de compte à compte</p>
+          </button>
+        </div>
+
+        {/* Segments */}
+        <IonSegment
+          value={selectedSegment}
+          onIonChange={(e) => setSelectedSegment(e.detail.value!)}
+          className="custom-segment"
+        >
+          <IonSegmentButton value="historique">
+            <IonLabel>Historique</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="a-signer">
+            <IonLabel>Virement à signer</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/* History Section */}
+        {selectedSegment === "historique" && (
+          <div className="history-section">
+            <h2>Historique</h2>
+            <IonSearchbar placeholder="Rechercher" className="custom-searchbar"></IonSearchbar>
+            <div className="transfers-list">{/* Add transfer history items here */}</div>
+          </div>
+        )}
+        
+      </IonContent>
+      <NavMobile currentPage="virements" />
+    </IonPage>
+  )
+}
+
+export default VirementsMobile
+
