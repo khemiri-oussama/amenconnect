c3d073b41b09117a77652c4e47f8640c1699418c
nexusMAKO1 <rayen1ouertani@gmail.com>
2025-02-19 02:20:45 +0100

    x

--- AmenConnect/src/pages/Home.tsx
+++ AmenConnect/src/pages/Home.tsx
@@ -24,12 +24,6 @@ const Home: React.FC = () => {
   // Detect if it's a borne interactive (not mobile, portrait, and touch screen)
   const isBorneInteractive = !isMobile && isPortrait && isTouchable
 
-  // Debugging: Log detection values
-  console.log("isMobile:", isMobile)
-  console.log("isPortrait:", isPortrait)
-  console.log("isTouchable:", isTouchable)
-  console.log("isBorneInteractive:", isBorneInteractive)
-
   return (
     <>
       {isMobile ? (
