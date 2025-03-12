d1a06f83636aab7ece7e38452c9b20492ad000e1
nexusMAKO1 <rayen1ouertani@gmail.com>
2025-03-13 00:41:57 +0100

    Increase request body size limits and remove captcha handling from account creation form

--- Backend/app.js
+++ Backend/app.js
@@ -36,7 +36,8 @@ const Twofa = require("./routes/2fa");
 const app = express();
 
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
