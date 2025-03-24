eb86ff0c98bfa304e1badb6146d4bd1a1302cc9e
khemiri-oussama <khemirioussama00@gmail.com>
2025-03-24 03:26:04 +0100

    coommyt

--- AmenConnect/src/components/JitsiMeetComponent.tsx
+++ AmenConnect/src/components/JitsiMeetComponent.tsx
@@ -8,10 +8,8 @@ const JitsiMeetComponent: React.FC = () => {
 
   // Tenant (API key) must match the "sub" claim in your JWT token.
   // Here it is: "vpas-magic-cookie-3aaa5c6dc3d342e0b40703fe93348e6a"
-  const tenant = "vpaas-magic-cookie-3aaa5c6dc3d342e0b40703fe93348e6a";
-  
-  // JWT token: Ensure this token is valid (not expired) and matches the tenant.
-  const token = "dBvWnZx1JtNOOx59pnzjQ6XI6wAOReBR296ZnJ3Z8eSg_OsYSqt4qcNkZqkdzhVOHPl581x7L9C9OawJyUhCcq3-wzTm8Grd0MC3Aq724LXPpXWNalCzOMemsXnM8zPV0QW3zsHbvxY8IptQe80n0a2OV_mptkG82lIAP-Sm9Z4plm8ZVZ5V3E4kEoXXT4KgkaPwT3vIBY8_0HE-5s900OOOcOLLcoM2mtpSZ7GxPwp5VkJLFx0PWsXcYSEA4oKQ3P-uLFaCrRU7EapM4Aj72s8kNWfq6daaIWwB_B4HlT3agrjXRhh4PdZqjXr3jLeTHjzLxy3e069UoF2eV6hXnQ";
+  const tenant = "vpaas-magic-cookie-3aaa5c6dc3d342e0b40703fe93348e6a" // Your JaaS tenant name (API key)
+  const token = "eyJraWQiOiJ2cGFhcy1tYWdpYy1jb29raWUtM2FhYTVjNmRjM2QzNDJlMGI0MDcwM2ZlOTMzNDhlNmEvZjk1MTY3LVNBTVBMRV9BUFAiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6ImNoYXQiLCJpYXQiOjE3NDI3ODE2NzcsImV4cCI6MTc0Mjc4ODg3NywibmJmIjoxNzQyNzgxNjcyLCJzdWIiOiJ2cGFhcy1tYWdpYy1jb29raWUtM2FhYTVjNmRjM2QzNDJlMGI0MDcwM2ZlOTMzNDhlNmEiLCJjb250ZXh0Ijp7ImZlYXR1cmVzIjp7ImxpdmVzdHJlYW1pbmciOnRydWUsIm91dGJvdW5kLWNhbGwiOnRydWUsInNpcC1vdXRib3VuZC1jYWxsIjpmYWxzZSwidHJhbnNjcmlwdGlvbiI6dHJ1ZSwicmVjb3JkaW5nIjp0cnVlfSwidXNlciI6eyJoaWRkZW4tZnJvbS1yZWNvcmRlciI6ZmFsc2UsIm1vZGVyYXRvciI6dHJ1ZSwibmFtZSI6ImtoZW1pcmlvdXNzYW1hMDAiLCJpZCI6Imdvb2dsZS1vYXV0aDJ8MTE2NDU1MjU3ODM1MTU1ODAzNTAzIiwiYXZhdGFyIjoiIiwiZW1haWwiOiJraGVtaXJpb3Vzc2FtYTAwQGdtYWlsLmNvbSJ9fSwicm9vbSI6IioifQ.dBvWnZx1JtNOOx59pnzjQ6XI6wAOReBR296ZnJ3Z8eSg_OsYSqt4qcNkZqkdzhVOHPl581x7L9C9OawJyUhCcq3-wzTm8Grd0MC3Aq724LXPpXWNalCzOMemsXnM8zPV0QW3zsHbvxY8IptQe80n0a2OV_mptkG82lIAP-Sm9Z4plm8ZVZ5V3E4kEoXXT4KgkaPwT3vIBY8_0HE-5s900OOOcOLLcoM2mtpSZ7GxPwp5VkJLFx0PWsXcYSEA4oKQ3P-uLFaCrRU7EapM4Aj72s8kNWfq6daaIWwB_B4HlT3agrjXRhh4PdZqjXr3jLeTHjzLxy3e069UoF2eV6hXnQ" // Your JWT token
 
   // Construct the base URL for your 8×8 JaaS meeting deployment
   const baseUrl = `https://8x8.vc/${tenant}/${secureRoomName}`;
