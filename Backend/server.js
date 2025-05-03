048c44dab2eac7f97f8d90686324e2ea797b9bcd
nexusMAKO1 <rayen1ouertani@gmail.com>
2025-05-03 01:49:40 +0100

    feat: refactor totem status monitoring for improved readability and consistency

--- Backend/server.js
+++ Backend/server.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 const http = require('http');
 const { initSocket } = require('./server/socket');
 const PORT = process.env.PORT || 3000;
-
+const startTotemMonitor = require('./monitorTotems');
 // Create an HTTP server with the Express app
 const server = http.createServer(app);
 
 // Initialize Socket.IO with the initSocket function from server/socket.js
 initSocket(server);
-
+startTotemMonitor();
 // Continue with your database connection and start the server
 connectDB()
   .then(() => {
